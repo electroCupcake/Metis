8054fb9f79d219e49f11605f9d134efda10fd05b
John Convertino <electrobs@gmail.com>
2016-12-03 10:25:35 -0500

    updated pysq, added dummy readmes to be filled out later

---
+++ src/util/GAME_BUILDER/gameBuild.h
@@ -0,0 +1,41 @@
+/*
+* Metis 2D Game Engine
+* 2016 John Convertino
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*
+* The full License is located in the root directory of this project, named LICENSE.txt.
+*
+* Developed By: John Convertino
+*
+* INFORMATION ABOUT THE HEADER AND ITS CONTENTS (WHAT IT ALLOWS YOU TO    * DO).
+*
+* Status: v0.0.0 -NADA
+* Previous
+*/
+
+#ifndef _GAME_BUILDER_H
+#define _GAME_BUILDER_H
+
+#include "types.h"
+
+//use this to setup xml, but if your using the object builder only call one or the other init
+void initGameBuilder();
+
+//parse the data
+void buildGame(char const *p_gameData);
+
+
+#endif 
